c5511224bc5580a30b6f33d0b1746dafd9569262
QuatschVirus <quatschvirus@gmail.com>
2021-07-31 17:43:15 +0200

    Added search to the Slime command

--- src/main/java/de/quatschvirus/essentialvirus/commands/SlimeCommand.java
+++ src/main/java/de/quatschvirus/essentialvirus/commands/SlimeCommand.java
@@ -1,12 +1,14 @@
 package de.quatschvirus.essentialvirus.commands;
 
 import de.quatschvirus.essentialvirus.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class SlimeCommand implements CommandExecutor {
@@ -17,9 +19,49 @@ public class SlimeCommand implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        if (args.length > 0) {
-            sender.sendMessage(Main.getPrefix() + ChatColor.RED + "Verwendung: /slime");
+        if (args.length > 1) {
+            sender.sendMessage(Main.getPrefix() + ChatColor.RED + "Verwendung: /slime oder /slime search");
             return true;
+        }
+        if (args.length > 0) {
+            if (args[0].equals("search")) {
+                int bounds = player.getClientViewDistance();
+                if (bounds > Bukkit.getViewDistance()) {
+                    bounds = Bukkit.getViewDistance();
+                }
+                for (int x = 0; x <= bounds; x++) {
+                    for (int z = 0; z <= bounds; z++) {
+                        long seed = player.getWorld().getSeed();
+                        int xPosition = player.getWorld().getChunkAt(player.getLocation()).getX() + x;
+                        int zPosition = player.getWorld().getChunkAt(player.getLocation()).getZ() + z;
+                        Random random = new Random(seed +
+                                ((long) xPosition * xPosition * 0x4c1906) +
+                                ((long) xPosition * 0x5ac0db) +
+                                ((long) zPosition * zPosition) * 0x4307a7L +
+                                ((long) zPosition * 0x5f24f) ^ 0x3ad8025f);
+                        if (random.nextInt(10) == 0) {
+                            player.sendMessage(Main.getPrefix() + ChatColor.AQUA + "Der Chunk " + xPosition + zPosition + " ist ein Slimechunk.");
+                            return false;
+                        }
+                        xPosition = player.getWorld().getChunkAt(player.getLocation()).getX() - x;
+                        zPosition = player.getWorld().getChunkAt(player.getLocation()).getZ() - z;
+                        random = new Random(seed +
+                                ((long) xPosition * xPosition * 0x4c1906) +
+                                ((long) xPosition * 0x5ac0db) +
+                                ((long) zPosition * zPosition) * 0x4307a7L +
+                                ((long) zPosition * 0x5f24f) ^ 0x3ad8025f);
+                        if (random.nextInt(10) == 0) {
+                            player.sendMessage(Main.getPrefix() + ChatColor.AQUA + "Der Chunk " + xPosition + zPosition + " ist ein Slimechunk.");
+                            return false;
+                        }
+                    }
+                }
+                player.sendMessage(Main.getPrefix() + ChatColor.RED + "Es wurde kein Chunk in deiner Umgebung gefunden");
+                return true;
+            } else {
+                sender.sendMessage(Main.getPrefix() + ChatColor.RED + "Verwendung: /slime oder /slime search");
+                return true;
+            }
         }
         long seed = player.getWorld().getSeed();
         int xPosition = player.getWorld().getChunkAt(player.getLocation()).getX();
@@ -37,3 +79,4 @@ public class SlimeCommand implements CommandExecutor {
         return false;
     }
 }
+
