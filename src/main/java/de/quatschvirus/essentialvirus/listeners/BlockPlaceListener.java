ecca14b42330baebf80b0b0a051f458f06719912
QuatschVirus <quatschvirus@gmail.com>
2021-12-19 12:07:15 +0100

    Additions for logging

--- src/main/java/de/quatschvirus/essentialvirus/listeners/BlockPlaceListener.java
+++ src/main/java/de/quatschvirus/essentialvirus/listeners/BlockPlaceListener.java
@@ -1,14 +1,21 @@
 package de.quatschvirus.essentialvirus.listeners;
 
+import de.quatschvirus.essentialvirus.Main;
+import de.quatschvirus.essentialvirus.utils.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import java.util.ArrayList;
+
 public class BlockPlaceListener implements Listener {
 
-    @SuppressWarnings({"SwitchStatementWithTooFewBranches", "ConstantConditions"})
+    @SuppressWarnings({"ConstantConditions"})
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         switch (event.getBlock().getType()) {
@@ -18,6 +25,17 @@ public class BlockPlaceListener implements Listener {
                 spawner.setSpawnedType(EntityType.valueOf(name));
                 spawner.update();
                 break;
+            }
+
+            case TNT: {
+                if (Config.contains("TNTBlock")) {
+                    ArrayList<String> blockedPlayers = (ArrayList<String>) Config.getStringList("TNTBlock");
+                    if (blockedPlayers.contains(event.getPlayer().getUniqueId().toString())) {
+                        event.getBlock().setType(Material.AIR);
+                        event.getPlayer().sendMessage(Main.getPrefix() + ChatColor.RED + "Es ist dir nicht gestattet, TNT zu platzieren!");
+                        Bukkit.getLogger().fine(event.getPlayer().getUniqueId() + " hat versucht, TNT zu platzieren!");
+                    }
+                }
             }
         }
     }
