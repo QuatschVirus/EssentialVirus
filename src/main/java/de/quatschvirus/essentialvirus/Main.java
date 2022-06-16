c2a85d0c987767f42b92b0c88968cd774fb8baec
QuatschVirus <quatschvirus@gmail.com>
2022-06-16 15:39:53 +0200

    Fixed locking

--- src/main/java/de/quatschvirus/essentialvirus/Main.java
+++ src/main/java/de/quatschvirus/essentialvirus/Main.java
@@ -72,6 +72,25 @@ public final class Main extends JavaPlugin {
             Material.YELLOW_SHULKER_BOX,
             Material.TRAPPED_CHEST
     );
+    public static List<Material> shulkerboxes = Arrays.asList(
+            Material.SHULKER_BOX,
+            Material.BLACK_SHULKER_BOX,
+            Material.BLUE_SHULKER_BOX,
+            Material.BROWN_SHULKER_BOX,
+            Material.CYAN_SHULKER_BOX,
+            Material.GRAY_SHULKER_BOX,
+            Material.GREEN_SHULKER_BOX,
+            Material.LIGHT_BLUE_SHULKER_BOX,
+            Material.LIGHT_GRAY_SHULKER_BOX,
+            Material.LIME_SHULKER_BOX,
+            Material.MAGENTA_SHULKER_BOX,
+            Material.ORANGE_SHULKER_BOX,
+            Material.PINK_SHULKER_BOX,
+            Material.PURPLE_SHULKER_BOX,
+            Material.RED_SHULKER_BOX,
+            Material.WHITE_SHULKER_BOX,
+            Material.YELLOW_SHULKER_BOX
+    );
 
     private boolean indev = false;
 
