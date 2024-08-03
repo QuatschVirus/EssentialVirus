4601d1cc6e05c43535db5e717c765b322564d33b
QuatschVirus <quatschvirus@gmail.com>
2024-08-04 00:47:10 +0200

    Util class to deal with Direction maths and magic

---
+++ src/main/java/de/quatschvirus/essentialvirus/utils/Directions.java
@@ -0,0 +1,29 @@
+package de.quatschvirus.essentialvirus.utils;
+
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.type.Stairs;
+
+import java.awt.*;
+
+public class Directions {
+    public static float Convert(BlockFace from) {
+        return switch (from) {
+            case NORTH -> 180;
+            case EAST -> -90;
+            case WEST -> 90;
+            default -> 0;
+        };
+    }
+
+    public static float ConvertStairsToFaceRotation(Stairs stairs) {
+        return Convert(stairs.getFacing()) + switch (stairs.getShape()) {
+            default -> 0;
+            case INNER_RIGHT, OUTER_RIGHT -> 45;
+            case INNER_LEFT, OUTER_LEFT -> -45;
+        };
+    }
+
+    public static float SnapToCardinal(float yaw) {
+        return Math.round(yaw / 90F) * 90F;
+    }
+}
